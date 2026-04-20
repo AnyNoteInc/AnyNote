bf1a45b31349f04dccfb8eb8c1d666905ced9825
Luferov Victor <luferovvs@yandex.ru>
2026-04-20 14:06:54 +0300

    feat(engines/infra): add qdrant module

--- apps/engines/src/app.module.ts
+++ apps/engines/src/app.module.ts
@@ -2,8 +2,9 @@ import { Module } from "@nestjs/common"
 
 import { HealthModule } from "./health/health.module.js"
 import { DbModule } from "./infra/db/db.module.js"
+import { QdrantModule } from "./infra/qdrant/qdrant.module.js"
 
 @Module({
-  imports: [DbModule, HealthModule],
+  imports: [DbModule, QdrantModule, HealthModule],
 })
 export class AppModule {}
